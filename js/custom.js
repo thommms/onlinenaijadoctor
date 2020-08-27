21bd7a8de50fed479a64571cff14a10596de2c1a
Thomas.Okonkwo <thomas.okonkwo@interswitchgroup.com>
2020-08-27 20:27:13 +0100

    Updates

--- js/custom.js
+++ js/custom.js
@@ -6,25 +6,6 @@
     $(".preloader").fadeOut(1000); // set duration in brackets
   });
 
-  // Submit form with id function.
-  function submit_by_id() {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    if (validation()) {
-      // Calling validation function
-      document.getElementById("appointment-form").submit(); //form submission
-      alert(
-        " Name : " +
-          name +
-          " n Email : " +
-          email +
-          " n Form Id : " +
-          document.getElementById("appointment-form").getAttribute("id") +
-          "nn Form Submitted Successfully......"
-      );
-    }
-  }
-
   //Navigation Section
   $(".navbar-collapse a").on("click", function () {
     $(".navbar-collapse").collapse("hide");
